c0fb557eb1a6e79a0c570ad098ecd86fb934757d
JimuelG <jimuelgaas@gmail.com>
2025-05-06 23:56:26 +0800

    Partial of Section 15(1)

--- client/src/environments/environment.development.ts
+++ client/src/environments/environment.development.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://localhost:5001/api/'
+    apiUrl: 'https://localhost:5001/api/',
+    stripePublicKey: 'pk_test_51RKhkaPNftC09szhaTe3x9ocZkXJFz93eavuNKvqzXArCRGJVg4hrSAtck3dEvoDIAfPE2suGXHP73sMhEG3jk1N00tyEo2jAh'
 };
